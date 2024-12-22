ee7c9c711d689c6edfa11103c9c25b9939d3f816
sadman sakib <sadmansakib1075@gmail.com>
2024-12-22 11:14:38 +0600

    added marque lost found

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
+import MarqueLostFound from "../MarqueLostFound/MarqueLostFound";
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <About></About>
+           <MarqueLostFound></MarqueLostFound>
         </div>
     );
 };
---
+++ src/pages/MarqueLostFound/MarqueLostFound.jsx
@@ -0,0 +1,85 @@
+import Marquee from "react-fast-marquee";
+import lost1 from "../../../public/lost1.jpg"
+import lost2 from "../../../public/lost2.jpg"
+import lost3 from "../../../public/lost3.jpg"
+import lost4 from "../../../public/lost4.jpg"
+import lost5 from "../../../public/lost5.jpg"
+import lost6 from "../../../public/lost6.jpg"
+import lost7 from "../../../public/lost7.jpg"
+
+const MarqueLostFound = () => {
+    return (
+        <div className='py-6'>
+        <div>
+            <p className='text-5xl font-bold text-center mb-10'>Lost And Found </p>
+        </div>
+        <Marquee className='mb-20'>
+            <div className="card card-compact bg-base-100 w-80 shadow-xl ml-5">
+                <figure>
+                    <img
+                        src={lost1}
+                        className="h-60 rounded-2xl"
+                        alt="Shoes" />
+                </figure>
+
+            </div>
+            <div className="card card-compact bg-base-100 w-80 shadow-xl ml-5">
+                <figure>
+                    <img
+                        src={lost2}
+                        className="h-60 rounded-2xl"
+                        alt="Shoes" />
+                </figure>
+
+            </div>
+            <div className="card card-compact bg-base-100 w-80 shadow-xl ml-5">
+                <figure>
+                    <img
+                        src={lost3}
+                        className="h-60 rounded-2xl"
+                        alt="Shoes" />
+                </figure>
+
+            </div>
+            <div className="card card-compact bg-base-100 w-80 shadow-xl ml-5">
+                <figure>
+                    <img
+                        src={lost4}
+                        className="h-60 rounded-2xl"
+                        alt="Shoes" />
+                </figure>
+
+            </div>
+            <div className="card card-compact bg-base-100 w-80 shadow-xl ml-5">
+                <figure>
+                    <img
+                        src={lost5}
+                        className="h-60 rounded-2xl"
+                        alt="Shoes" />
+                </figure>
+
+            </div>
+            <div className="card card-compact bg-base-100 w-80 shadow-xl ml-5">
+                <figure>
+                    <img
+                        src={lost6}
+                        className="h-60 rounded-2xl"
+                        alt="Shoes" />
+                </figure>
+
+            </div>
+            <div className="card card-compact bg-base-100 w-80 shadow-xl ml-5">
+                <figure>
+                    <img
+                        src={lost7}
+                        className="h-60 rounded-2xl"
+                        alt="Shoes" />
+                </figure>
+
+            </div>
+        </Marquee>
+    </div>
+    );
+};
+
+export default MarqueLostFound;
